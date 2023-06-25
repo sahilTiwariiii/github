eb004e98c9227c0c9413c744fa4d0faf125ae238
Sahil Tiwari <sahiltiwari1222@gmail.com>
2023-06-25 22:47:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
+import React from 'react';
 import './App.css';
-import UseEffectApi from './components/UseEffectApi';
+import {useSelector,useDispatch} from "react-redux"
+import {incNumber,decNumber} from './actions/index'
 
 function App() {
+  const myState= useSelector((state)=>state.changeTheNumber)
+  const dispatch=useDispatch()
   return (
-    <div className="App">
-      
-      <UseEffectApi/>
-    </div>
+    <>
+<div className="container">
+  <h1>Increment /Decrement counter</h1>
+  <h4>Using React and Redux</h4>
+  <div className="quantity">
+    <a  onClick={()=>dispatch(decNumber())}><span>-</span></a>
+    <input type="text" className="quantity_input" value={myState} />
+    <a  onClick={()=>dispatch(incNumber())}><span>+</span></a>
+  </div>
+</div>
+    </>
   );
 }
 
